74a46c16be8a92f8f5e0fcd03a41399ff97b458d
George Jauga <georgejauga37@gmail.com>
2022-06-12 01:14:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me'
+  } else if (distance >2000 && distance < 2500) {
+    return 'I would gladly take your thirty bucks'
+  } else if (distance > 2500){
+    return 'No can do'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go'
+  }
+  city === 'NYC' ? 'Ok, sounds good' : 'No go'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString){
+  if (tipString === 'generous'){
+    return 'Thank you so much.'
+  }else if (tipString === 'not as generoud'){
+    return 'Thank you'
+  }else if (tipString === 'thanks for everything'){
+    return 'Bye'
+  }
 }
